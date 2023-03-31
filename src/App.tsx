1a734cedb01e371be76c053c4e498b24b4b6a587
etctera <yilonglei790@gmail.com>
2023-03-31 17:57:06 +0800

    chore: oss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { useMutation } from '@apollo/client';
-import { Button, Form, Input } from 'antd-mobile';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Button, Form, Input, ImageUploader } from 'antd-mobile';
 import classNames from 'classnames';
 import { useEffect } from 'react';
 import { UPDATE } from './graphql/demo';
+import { useUploadOSS } from './hooks/useUploadOSS';
 import styles from './App.module.less';
 
 const App = () => {
   const [update] = useMutation(UPDATE);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const uploadHandler = useUploadOSS();
 
   useEffect(() => {
     document.documentElement.setAttribute('data-prefers-color-scheme', 'dark');
@@ -15,7 +19,7 @@ const App = () => {
   const onClickHandler = (v: any) => {
     update({
       variables: {
-        id: 'cb71e40d-9f15-40ef-a137-1acaa38831f4',
+        id: 'ab42a458-700f-45e5-aa7e-f458ce1043c5',
         params: {
           ...v,
         },
@@ -41,9 +45,9 @@ const App = () => {
         <Form.Item name="desc" label="描述">
           <Input />
         </Form.Item>
-        <Form.Item name="actor" label="头像">
-          {/* <ImageUploader upload={uploadHandler} /> */}
-        </Form.Item>
+        {/* <Form.Item name="actor" label="头像">
+          <ImageUploader upload={uploadHandler} />
+        </Form.Item> */}
       </Form>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import { ApolloProvider } from '@apollo/client';
+import { ConfigProvider } from 'antd-mobile';
+import zhCN from 'antd-mobile/es/locales/zh-CN';
 import { client } from './utils/apollo';
+
 import App from './App';
+import './theme.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <ConfigProvider locale={zhCN}>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </ConfigProvider>,
 );
